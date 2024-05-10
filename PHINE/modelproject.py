9b20f91a76c2926a051afdf1de57db7b21245d84
IsabellaGW <nlv483@alumni.ku.dk>
2024-05-10 16:00:17 +0200

    OPdateret

--- PHINE/modelproject.py
+++ PHINE/modelproject.py
@@ -29,3 +29,43 @@ def solow_equations(variables, s, n, g, delta, alpha):
     # Return equations
     return solow_k
 
+def multi_start(num_guesses=100, bounds=[1e-5, 50], fun=solow_equations, args= None, method='hybr'):
+    """
+    Performs multi-start optimization to find the steady state solutions for k.
+    
+    Args:
+        num_guesses     (int): The number of random initial guesses, default = 100
+        bounds        (tuple): The bounds for the random initial guesses, default = [1e-5, 50]
+        fun        (function): The function to be optimized, default = n_ss_solow
+        args          (tuple): The tuple of arguments for the function, default = None
+        method       (method): The optimization method to use, default = 'hybr'
+    
+    Returns:
+        The steady state values for k and y, and the residual of the function
+    """
+    # Initialize the smallest residual as infinity
+    smallest_residual = np.inf
+
+    # Generate a list of random numbers within the specified bounds
+    random_samples = list(np.random.uniform(low=bounds[0], high=bounds[1], size=num_guesses))
+
+
+
+    # Loop through each random initial guess
+    for i in range(num_guesses):
+        # Select a random pair of numbers from the list of random samples
+        initial_guess = random.sample(random_samples, 2)
+
+        # Solve the optimization problem with the current initial guess
+        sol = optimize.root(fun = fun, x0 = initial_guess, args = args, method = method)
+        
+        # Calculate the residual norm (Euclidean norm) of the current solution
+        residual_norm = np.linalg.norm(sol.fun)
+
+        # If the residual norm is smaller than the current smallest residual, update the steady state of k and h and the smallest residual
+        if residual_norm < smallest_residual:
+            smallest_residual = residual_norm
+            steady_state_k = sol.x
+    
+    # Return optimal solutions 
+    return steady_state_k, smallest_residual
