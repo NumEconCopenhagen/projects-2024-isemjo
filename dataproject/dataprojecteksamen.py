84776efe38a9bd356f2ea0885dbfb2c72dfded46
IsabellaGW <nlv483@alumni.ku.dk>
2024-06-23 14:04:10 +0200

    syn

--- dataproject/dataprojecteksamen.py
+++ dataproject/dataprojecteksamen.py
@@ -1,26 +1,14 @@
-def keep_regs(df, regs):
-    """ Example function. Keep only the subset regs of regions in data.
 
-    Args:
-        df (pd.DataFrame): pandas dataframe 
 
-    Returns:
-        df (pd.DataFrame): pandas dataframe
-
-    """ 
-    
-    for r in regs:
-        I = df.reg.str.contains(r)
-        df = df.loc[I == False] # keep everything else
-    
-    return df
-
-import seaborn as sns
-import matplotlib.pyplot as plt
 import pandas as pd
-import plotly.express as px
+import numpy as np
 import ipywidgets as widgets
+import warnings
+import seaborn as sns
+import plotly.express as px
 from IPython.display import display
+
+import matplotlib.pyplot as plt
 
 #The following plots and dropboxes are made using OpenAI Chatgpt 4o:
 
